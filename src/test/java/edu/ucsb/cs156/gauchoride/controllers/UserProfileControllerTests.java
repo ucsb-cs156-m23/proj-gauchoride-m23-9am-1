6630f29c23bb9f3dd09b45d29b3267f6c6266b4e
Aasish Virjala <aasish@ucsb.edu>
2023-08-17 16:22:06 -0700

    added lines to fix UserProfileControllerTest and passes pitest and line coverage

--- src/test/java/edu/ucsb/cs156/gauchoride/controllers/UserProfileControllerTests.java
+++ src/test/java/edu/ucsb/cs156/gauchoride/controllers/UserProfileControllerTests.java
@@ -7,6 +7,10 @@ import edu.ucsb.cs156.gauchoride.repositories.UserRepository;
 import edu.ucsb.cs156.gauchoride.testconfig.TestConfig;
 import org.springframework.http.MediaType;
 
+import edu.ucsb.cs156.gauchoride.entities.User;
+
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -14,9 +18,16 @@ import org.springframework.context.annotation.Import;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MvcResult;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 
 @WebMvcTest(controllers = UserProfileController.class)
 @Import(TestConfig.class)
@@ -25,11 +36,20 @@ public class UserProfileControllerTests extends ControllerTestCase {
   @MockBean
   UserRepository userRepository;
 
-@Test
-    public void logged_out_users_cannot_edit_profile() throws Exception {
-            mockMvc.perform(put("/api/userprofile?cellPhone=1234"))
-                            .andExpect(status().is(403));
-}
+    @Test
+        public void logged_out_users_cannot_edit_profile() throws Exception {
+                mockMvc.perform(put("/api/userprofile?cellPhone=1234"))
+                                .andExpect(status().is(403));
+    }
+
+    @WithMockUser(roles={"USER"})
+    @Test
+        public void logged_in_users_cannot_edit_profile_that_does_not_exist() throws Exception {
+                when(userRepository.findById(eq(1L))).thenReturn(Optional.empty());
+                MvcResult response = mockMvc.perform(
+                    put("/api/userprofile?cellPhone=1234").with(csrf())).andExpect(status().isNotFound()).andReturn();
+                verify(userRepository, times(1)).findById(1L);
+    }
 
   @WithMockUser(roles = { "USER" })
   @Test
@@ -38,13 +58,48 @@ public class UserProfileControllerTests extends ControllerTestCase {
     // arrange
 
     CurrentUser currentUser = currentUserService.getCurrentUser();
-    String expectedJson = mapper.writeValueAsString(currentUser);
+
+    User fakeUserOld = User.builder()
+        .id(1)
+        .email("test@ucsb.edu")
+        .cellPhone("123456789")
+        .googleSub("None")
+        .pictureUrl("None")
+        .fullName("Test User 1")
+        .givenName("Test")
+        .familyName("User 1")
+        .emailVerified(true)
+        .locale("UCSB")
+        .hostedDomain("None")
+        .admin(false)
+        .driver(false)
+        .rider(true)
+        .build();
+    User fakeUserNew = User.builder()
+        .id(1)
+        .email("test@ucsb.edu")
+        .cellPhone("1234")
+        .googleSub("None")
+        .pictureUrl("None")
+        .fullName("Test User 1")
+        .givenName("Test")
+        .familyName("User 1")
+        .emailVerified(true)
+        .locale("UCSB")
+        .hostedDomain("None")
+        .admin(false)
+        .driver(false)
+        .rider(true)
+        .build();
+
+    String expectedJson = mapper.writeValueAsString(fakeUserNew);
+
+    when(userRepository.findById(eq(1L))).thenReturn(Optional.of(fakeUserOld));
 
     // act
 
     MvcResult response = mockMvc.perform(
-                    put("/api/userprofile?cellPhone=1234")
-                                    .characterEncoding("utf-8")).andExpect(status().isOk()).andReturn();
+                    put("/api/userprofile?cellPhone=1234").with(csrf())).andExpect(status().isOk()).andReturn();
 
     // assert
     String responseString = response.getResponse().getContentAsString();
