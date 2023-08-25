c0ea682032f2dc1adecc42d7ba34748d85236a79
Pablo <pds@ucsb.edu>
2023-08-25 03:18:54 -0700

    driver dashboard contains all your own shifts

--- frontend/src/tests/pages/Drivers/driverDashboardPage.test.js
+++ frontend/src/tests/pages/Drivers/driverDashboardPage.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
 import DriverDashboardPage from "main/pages/Drivers/driverDashboardPage";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
@@ -7,10 +7,35 @@ import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
 import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
+import shiftFixtures from "fixtures/shiftFixtures";
+import mockConsole from "jest-mock-console";
 
-describe("PlaceholderCreatePage tests", () => {
 
-    const axiosMock = new AxiosMockAdapter(axios);
+const mockToast = jest.fn();
+jest.mock('react-toastify', () => {
+    const originalModule = jest.requireActual('react-toastify');
+    return {
+        __esModule: true,
+        ...originalModule,
+        toast: (x) => mockToast(x)
+    };
+});
+
+const mockNavigate = jest.fn();
+jest.mock('react-router-dom', () => {
+    const originalModule = jest.requireActual('react-router-dom');
+    return {
+        __esModule: true,
+        ...originalModule,
+        Navigate: (x) => { mockNavigate(x); return null; }
+    };
+});
+
+describe("DriverDashBoardPage tests", () => {
+
+    const axiosMock =new AxiosMockAdapter(axios);
+
+    const testId = "ShiftTable";
 
     const setupUserOnly = () => {
         axiosMock.reset();
@@ -19,13 +44,25 @@ describe("PlaceholderCreatePage tests", () => {
         axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
     };
 
-    const queryClient = new QueryClient();
-    test("Renders expected content", () => {
-        // arrange
+    const setupDriverOnly = () => {
+        axiosMock.reset();
+        axiosMock.resetHistory();
+        axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.driverOnly);
+        axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
+    };
 
-        setupUserOnly();
-       
-        // act
+    const setupAdminUser = () => {
+        axiosMock.reset();
+        axiosMock.resetHistory();
+        axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.adminUser);
+        axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
+    };
+
+    test("renders without crashing for driver", () => {
+        setupDriverOnly();
+        const queryClient = new QueryClient();
+        axiosMock.onGet("/api/shift/drivershifts").reply(200, []);
+
         render(
             <QueryClientProvider client={queryClient}>
                 <MemoryRouter>
@@ -34,8 +71,49 @@ describe("PlaceholderCreatePage tests", () => {
             </QueryClientProvider>
         );
 
-        // assert
-        expect(screen.getByText("Driver Dashboard Page not yet implemented")).toBeInTheDocument();
+
     });
+
+
+    test("renders three rides without crashing for driver", async () => {
+        setupDriverOnly();
+        const queryClient = new QueryClient();
+        axiosMock.onGet("/api/shift/drivershifts").reply(200, shiftFixtures.threeShifts);
+
+        const { getByText } = render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <DriverDashboardPage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+
+        await waitFor(() => expect(getByText("Shift")).toBeInTheDocument());
+
+
+    });
+
+    test("renders empty table when backend unavailable", async () => {
+        const queryClient = new QueryClient();
+        axiosMock.onGet("/api/shift/drivershifts").timeout();
+
+        const restoreConsole = mockConsole();
+
+        const { queryByTestId } = render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <DriverDashboardPage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+
+        await waitFor(() => { expect(axiosMock.history.get.length).toBeGreaterThanOrEqual(1); });
+        restoreConsole();
+
+        expect(queryByTestId(`${testId}-cell-row-0-col-id`)).not.toBeInTheDocument();
+
+    });
+
+ 
 
 });
