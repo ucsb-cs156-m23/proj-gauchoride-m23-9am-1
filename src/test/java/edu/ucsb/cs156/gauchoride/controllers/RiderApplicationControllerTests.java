7a2eeaec71624b29176a875432938634bb6e3412
pabucsb <97569267+pabucsb@users.noreply.github.com>
2023-08-27 11:57:22 -0700

    Update src/test/java/edu/ucsb/cs156/gauchoride/controllers/RiderApplicationControllerTests.java

--- src/test/java/edu/ucsb/cs156/gauchoride/controllers/RiderApplicationControllerTests.java
+++ src/test/java/edu/ucsb/cs156/gauchoride/controllers/RiderApplicationControllerTests.java
@@ -91,7 +91,7 @@ public class RiderApplicationControllerTests extends ControllerTestCase {
 
     @WithMockUser(roles = { "ADMIN" })
     @Test
-    public void logged_in_admins_can_get_by_id() throws Exception {
+    public void logged_in_admins_cant_get_by_id() throws Exception {
         mockMvc.perform(get("/api/riderapplication/get?id=7"))
                             .andExpect(status().is(404)); 
     }
