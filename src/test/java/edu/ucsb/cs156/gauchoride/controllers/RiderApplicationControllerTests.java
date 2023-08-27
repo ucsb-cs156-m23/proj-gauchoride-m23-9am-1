86d31b621e9cdef68d0cedf368557a3fa754ae2b
pabucsb <97569267+pabucsb@users.noreply.github.com>
2023-08-27 11:57:56 -0700

    Update src/test/java/edu/ucsb/cs156/gauchoride/controllers/RiderApplicationControllerTests.java

--- src/test/java/edu/ucsb/cs156/gauchoride/controllers/RiderApplicationControllerTests.java
+++ src/test/java/edu/ucsb/cs156/gauchoride/controllers/RiderApplicationControllerTests.java
@@ -100,7 +100,7 @@ public class RiderApplicationControllerTests extends ControllerTestCase {
     @Test
     public void logged_in_drivers_cant_get_by_id() throws Exception {
         mockMvc.perform(get("/api/riderapplication/get?id=7"))
-                            .andExpect(status().is(404)); 
+                            .andExpect(status().is(403)); 
     }
 
     @Test
