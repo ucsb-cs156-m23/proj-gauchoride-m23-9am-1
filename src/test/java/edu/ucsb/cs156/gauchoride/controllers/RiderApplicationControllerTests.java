7100b509c21db8678ae5fc8b24a4c35a166f6f0a
pabucsb <97569267+pabucsb@users.noreply.github.com>
2023-08-27 11:56:32 -0700

    Update src/test/java/edu/ucsb/cs156/gauchoride/controllers/RiderApplicationControllerTests.java

--- src/test/java/edu/ucsb/cs156/gauchoride/controllers/RiderApplicationControllerTests.java
+++ src/test/java/edu/ucsb/cs156/gauchoride/controllers/RiderApplicationControllerTests.java
@@ -58,7 +58,7 @@ public class RiderApplicationControllerTests extends ControllerTestCase {
     @Test
     public void logged_admin_users_cant_get_all_of_theirs() throws Exception {
         mockMvc.perform(get("/api/riderapplication/all"))
-                            .andExpect(status().is(200)); 
+                            .andExpect(status().is(403)); 
     }
 
     @WithMockUser(roles = { "DRIVER" })
